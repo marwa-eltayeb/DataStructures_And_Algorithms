0ac973c4fddd79aaa86de784ae6e6f864f94d031
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-04-09 21:56:26 +0200

    Bubble Sort

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -9,11 +9,12 @@ public class BubbleSort {
     public static void main(String[] args) {
         
         // Implement Bubble Sort
-        int[] array = {1, 50, 30, 10, 60, 80};
+        int[] array = {10, 4, 3, 5, 7, 9};
         bubbleSort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "\t");
         }
+      
     }
     
     public static void bubbleSort(int[] array) {
@@ -22,8 +23,8 @@ public class BubbleSort {
         
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
-                // If first elemnt is greater than second element, swap              
-                if (array[j - 1] > array[j]) {
+                // If current elemnt is greater than next element, swap              
+                if (array[j - 1] > array[j]) {  
                     temp = array[j - 1];
                     array[j - 1] = array[j];
                     array[j] = temp;
