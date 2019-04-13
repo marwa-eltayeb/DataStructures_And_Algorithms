28b12af7c50c27e33cb0e130dd6e12121d8e24f2
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-04-13 20:05:04 +0200

    Selection Sort

---
+++ src/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package algorithms.sorting;
+
+/**
+ *
+ * @author Marwa
+ */
+public class SelectionSort {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    static void selectionSort(int[] arr){
+	for(int i=0;i<arr.length-1;i++){
+		int index=i;
+		for(int j=i+1;j<arr.length;j++){
+			if( arr[j]<arr[index])
+				index=j;
+		}
+		if(index!=i){
+			int temp= arr[index];
+			arr[index]=arr[i];
+			arr[i]=temp;
+		}
+	}
+}
+    
+}
